42a111b4c879e70f871b2e9d340daac5549703e6
uliadib12 <uliadib@gmail.com>
2021-08-06 06:19:36 +0700

    menambakan layout

--- app/src/main/java/com/example/firstgit/MainActivity.java
+++ app/src/main/java/com/example/firstgit/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.firstgit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private View layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        layout = findViewById(R.id.constraint);
     }
 }
