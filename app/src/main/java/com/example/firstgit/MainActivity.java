5244ae1e19c64992877f2d552ff063dc0a9cd51c
uliadib12 <uliadib@gmail.com>
2021-08-06 06:51:33 +0700

    a = 100

--- app/src/main/java/com/example/firstgit/MainActivity.java
+++ app/src/main/java/com/example/firstgit/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        a = 2;
+        a = 100;
     }
 }
