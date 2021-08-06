547cf9fa0c329a06f5b71088c7b6f924c9cf66fd
uliadib12 <uliadib@gmail.com>
2021-08-06 22:06:36 +0700

    Layout Quiz

--- app/src/main/java/com/example/firstgit/MainActivity.java
+++ app/src/main/java/com/example/firstgit/MainActivity.java
@@ -7,11 +7,9 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     private View layout;
-    private int b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        b = 10;
     }
 }
