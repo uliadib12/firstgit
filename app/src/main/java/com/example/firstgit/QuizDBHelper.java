65ee4a29bbab8c007ffd731bbea3e626fcbf87e7
uliadib12 <uliadib@gmail.com>
2021-08-07 00:54:59 +0700

    Layout kacau

--- app/src/main/java/com/example/firstgit/QuizDBHelper.java
+++ app/src/main/java/com/example/firstgit/QuizDBHelper.java
@@ -2,64 +2,83 @@ package com.example.firstgit;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import com.example.firstgit.QuizContract.*;
 
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class QuizDBHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "MyAwesomeQuiz.db";
     private static final int DATABASE_VERSION = 1;
     private SQLiteDatabase db;
+
     public QuizDBHelper(@Nullable Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        super(context,DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
         this.db = db;
-        final String SQL_CREATE_QUESTION_TABLE = "CREATE TABLE " +
+        final String SQL_CREATE_QUESTIONS_TABLE = "CREATE TABLE " +
                 QuestionTable.TABLE_NAME + " ( " +
                 QuestionTable._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                QuestionTable.COLUMN_QUESTION + " TEXT," +
-                QuestionTable.COLUMN_OPTION1 + " TEXT," +
-                QuestionTable.COLUMN_OPTION2 + " TEXT," +
-                QuestionTable.COLUMN_OPTION3 + " TEXT," +
-                QuestionTable.COLUMN_OPTION4 + " TEXT," +
+                QuestionTable.COLUMN_QUESTION + " TEXT, " +
+                QuestionTable.COLUMN_OPTION1 + " TEXT, " +
+                QuestionTable.COLUMN_OPTION2 + " TEXT, " +
+                QuestionTable.COLUMN_OPTION3 + " TEXT, " +
                 QuestionTable.COLUMN_ANSWER_NR + " INTEGER" +
-                " )"
-                ;
-        db.execSQL(SQL_CREATE_QUESTION_TABLE);
-        fillQuestionTable();
+                ")";
+        db.execSQL(SQL_CREATE_QUESTIONS_TABLE);
+        fillQuestionsTable();
     }
-
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + QuestionTable.TABLE_NAME);
         onCreate(db);
     }
-
-    private void fillQuestionTable(){
-        Question q1 = new Question("A is Correct","A","B","C","D",1);
+    private void fillQuestionsTable() {
+        Question q1 = new Question("A is correct", "A", "B", "C","D", 1);
         addQuestion(q1);
-        Question q2 = new Question("A is Correct","A","B","C","D",1);
+        Question q2 = new Question("B is correct", "A", "B", "C","D", 2);
         addQuestion(q2);
-        Question q3 = new Question("A is Correct","A","B","C","D",1);
+        Question q3 = new Question("C is correct", "A", "B", "C","D", 3);
         addQuestion(q3);
-        Question q4 = new Question("A is Correct","A","B","C","D",1);
+        Question q4 = new Question("D is correct", "A", "B", "C","D", 4);
         addQuestion(q4);
-        Question q5 = new Question("A is Correct","A","B","C","D",1);
+        Question q5 = new Question("D is correct", "A", "B", "C","D", 4);
         addQuestion(q5);
     }
-    private void addQuestion(Question question){
+    private void addQuestion(Question question) {
         ContentValues cv = new ContentValues();
-        cv.put(QuestionTable.COLUMN_QUESTION,question.getQuestion());
-        cv.put(QuestionTable.COLUMN_OPTION1,question.getOptions1());
-        cv.put(QuestionTable.COLUMN_OPTION2,question.getOptions2());
-        cv.put(QuestionTable.COLUMN_OPTION3,question.getOptions3());
-        cv.put(QuestionTable.COLUMN_OPTION4,question.getOptions4());
-        cv.put(QuestionTable.COLUMN_ANSWER_NR,question.getAnswerNr());
-        db.insert(QuestionTable.TABLE_NAME,null,cv);
+        cv.put(QuestionTable.COLUMN_QUESTION, question.getQuestion());
+        cv.put(QuestionTable.COLUMN_OPTION1, question.getOptions1());
+        cv.put(QuestionTable.COLUMN_OPTION2, question.getOptions1());
+        cv.put(QuestionTable.COLUMN_OPTION3, question.getOptions1());
+        cv.put(QuestionTable.COLUMN_ANSWER_NR, question.getAnswerNr());
+        db.insert(QuestionTable.TABLE_NAME, null, cv);
+    }
+    public List<Question> getAllQuestions() {
+        List<Question> questionList = new ArrayList<>();
+        db = getReadableDatabase();
+        Cursor c = db.rawQuery("SELECT * FROM " + QuestionTable.TABLE_NAME, null);
+        if (c.moveToFirst()) {
+            do {
+                Question question = new Question();
+                question.setQuestion(c.getString(c.getColumnIndex(QuestionTable.COLUMN_QUESTION)));
+                question.setOptions1(c.getString(c.getColumnIndex(QuestionTable.COLUMN_OPTION1)));
+                question.setOptions2(c.getString(c.getColumnIndex(QuestionTable.COLUMN_OPTION2)));
+                question.setOptions3(c.getString(c.getColumnIndex(QuestionTable.COLUMN_OPTION3)));
+                question.setOptions4(c.getString(c.getColumnIndex(QuestionTable.COLUMN_OPTION4)));
+                question.setAnswerNr(c.getInt(c.getColumnIndex(QuestionTable.COLUMN_ANSWER_NR)));
+                questionList.add(question);
+            } while (c.moveToNext());
+        }
+        c.close();
+        return questionList;
     }
 }
